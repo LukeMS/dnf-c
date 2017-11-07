e04097602721aa09cd6d773e75a2d22241c65247
Lucas de Morais Siqueira <lucas.morais.siqueira@gmail.com>
2017-11-07 17:36:11 -0200

    travis

---
+++ git-commit.bat
@@ -0,0 +1,9 @@
+:git remote add origin https://github.com/LukeMS/dnf-c.git
+@git config --global user.email "lucas.morais.siqueira@gmail.com"
+@git config --global user.name "Lucas de Morais Siqueira"
+@git add --all
+@set /p msg="Describe the commit: "
+@git commit -m "%msg%"
+@git push origin master
+
+@pause
