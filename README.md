9a5c0aae61ada35cee0873f1dd2f4684c1835acf
Lucas de Morais Siqueira <lucas.morais.siqueira@gmail.com>
2017-11-08 09:42:36 -0200

    travis

---
+++ README.md
@@ -0,0 +1,2 @@
+# dnf-c [![Build Status](https://travis-ci.org/LukeMS/dnf-c.svg?branch=master)](https://travis-ci.org/LukeMS/dnf-c)
+DNF in C
--- git-commit.bat
+++ git-commit.bat
@@ -1,9 +1,13 @@
 :git remote add origin https://github.com/LukeMS/dnf-c.git
-@git config --global user.email "lucas.morais.siqueira@gmail.com"
-@git config --global user.name "Lucas de Morais Siqueira"
+:@git config --global user.email "lucas.morais.siqueira@gmail.com"
+:@git config --global user.name "Lucas de Morais Siqueira"
+
 @git add --all
-@set /p msg="Describe the commit: "
-@git commit -m "%msg%"
+
+:@set /p msg="Describe the commit: "
+:@git commit -m "%msg%"
+
+@git commit -m "travis"
 @git push origin master
 
 @pause
